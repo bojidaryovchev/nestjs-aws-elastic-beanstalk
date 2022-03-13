f48f11b009872bc780a7ba60279c9f4f9ce1fc9d
bojidaryovchev <blizzardcon10@gmail.com>
2022-03-13 04:50:37 +0200

    feat: Prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getHello(): Promise<any> {
+    return await this.prismaService.user.findMany();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { PrismaService } from 'src/prisma.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
